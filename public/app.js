409b02b7d08b472c97e15f64cf3b28a0436d9a5f
Emmanuel Amicel <emmanuel.amicel@gmail.com>
2023-08-13 17:25:44 +0200

    Replace buttons by arrow and add animation

--- public/app.js
+++ public/app.js
@@ -2,8 +2,8 @@ class ClipPlayer {
   constructor() {
     this.currentClipIndex = 0
 
-    this.prevButton = document.getElementById('prevButton')
-    this.nextButton = document.getElementById('nextButton')
+    this.prevButton = document.getElementById('topArrow')
+    this.nextButton = document.getElementById('bottomArrow')
     this.currentClipFrame = document.getElementById('currentClip')
 
     this.prevButton.addEventListener('click', this.showPreviousClip.bind(this))
@@ -11,39 +11,59 @@ class ClipPlayer {
 
     this.getClips().then((clips) => {
       this.clips = clips
-      this.displayClip(this.currentClipIndex)
+      this.displayClip(this.currentClipIndex, null)
     })
   }
 
   async getClips() {
     try {
-      const response = await fetch('/clips');
-      const clips = await response.json();
-      return clips;
+      const response = await fetch('/clips')
+      const clips = await response.json()
+      return clips
     } catch (err) {
-      console.error('Error while retrieving clips: ' + err);
+      console.error('Error while retrieving clips: ' + err)
     }
   }
 
-  displayClip(clipIndex) {
+  async displayClip(clipIndex, transition) {
     this.currentClipFrame.src = `${this.clips[clipIndex].embed_url}&parent=localhost&autoplay=false&muted=false`
-  }
-
-  showPreviousClip() {
-    console.log('showPreviousClip')
-    if (this.currentClipIndex > 0) {
-      this.currentClipIndex--
-      this.displayClip(this.currentClipIndex)
-      console.log(this.currentClipIndex)
+    
+    if (transition === 'slide-in-up') {
+      this.currentClipFrame.classList.add('slide-in-up')
+      this.currentClipFrame.addEventListener('animationend', () => {
+        this.currentClipFrame.classList.remove('slide-in-up')
+      })
+    } else if (transition === 'slide-in-down') {
+      this.currentClipFrame.classList.add('slide-in-down')
+      this.currentClipFrame.addEventListener('animationend', () => {
+        this.currentClipFrame.classList.remove('slide-in-down')
+      })
     }
   }
 
-  showNextClip() {
-    console.log('showNextClip')
+  async showPreviousClip() {
+    if (this.currentClipIndex > 0) {
+      const previousClipIndex = this.currentClipIndex - 1
+
+      this.currentClipFrame.classList.add('slide-out-down')
+      this.currentClipFrame.addEventListener('animationend', () => {
+        this.currentClipFrame.classList.remove('slide-out-down')
+        this.currentClipIndex = previousClipIndex
+        this.displayClip(this.currentClipIndex, 'slide-in-down')
+      })
+    }
+  }
+
+  async showNextClip() {
     if (this.currentClipIndex < this.clips.length - 1) {
-      this.currentClipIndex++
-      this.displayClip(this.currentClipIndex)
-      console.log(this.currentClipIndex)
+      const nextClipIndex = this.currentClipIndex + 1
+
+      this.currentClipFrame.classList.add('slide-out-up')
+      this.currentClipFrame.addEventListener('animationend', () => {
+        this.currentClipFrame.classList.remove('slide-out-up')
+        this.currentClipIndex = nextClipIndex
+        this.displayClip(this.currentClipIndex, 'slide-in-up')
+      })
     }
   }
 }
