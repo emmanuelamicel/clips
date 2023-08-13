f6b10f40c006890375dc92a66085b6206a059966
Emmanuel Amicel <emmanuel.amicel@gmail.com>
2023-08-13 14:37:20 +0200

    Add buttons to change current clip

--- public/app.js
+++ public/app.js
@@ -1,27 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-  async function getClips() {
-    try {
-      const response = await fetch('/clips')
-      const clips = await response.json()
-      const firstClip = clips[0]
+class ClipPlayer {
+  constructor() {
+    this.currentClipIndex = 0
 
-      const container = document.getElementById('clip')
-      const clipHTML = `
-        <iframe
-          src="${firstClip.embed_url}&parent=localhost&autoplay=true&muted=false"
-          height="480"
-          width="854"
-          frameborder="0"
-          allowfullscreen
-          autoplay=true>
-        ></iframe>
-      `
-      container.innerHTML = clipHTML
-    }
-    catch (err) {
-      console.error('Error while retrieving clips: ' + err)
+    this.prevButton = document.getElementById('prevButton')
+    this.nextButton = document.getElementById('nextButton')
+    this.currentClipFrame = document.getElementById('currentClip')
+
+    this.prevButton.addEventListener('click', this.showPreviousClip.bind(this))
+    this.nextButton.addEventListener('click', this.showNextClip.bind(this))
+
+    this.getClips().then((clips) => {
+      this.clips = clips
+      this.displayClip(this.currentClipIndex)
+    })
+  }
+
+  async getClips() {
+    try {
+      const response = await fetch('/clips');
+      const clips = await response.json();
+      return clips;
+    } catch (err) {
+      console.error('Error while retrieving clips: ' + err);
     }
   }
 
-  getClips()
+  displayClip(clipIndex) {
+    this.currentClipFrame.src = `${this.clips[clipIndex].embed_url}&parent=localhost&autoplay=false&muted=false`
+  }
+
+  showPreviousClip() {
+    console.log('showPreviousClip')
+    if (this.currentClipIndex > 0) {
+      this.currentClipIndex--
+      this.displayClip(this.currentClipIndex)
+      console.log(this.currentClipIndex)
+    }
+  }
+
+  showNextClip() {
+    console.log('showNextClip')
+    if (this.currentClipIndex < this.clips.length - 1) {
+      this.currentClipIndex++
+      this.displayClip(this.currentClipIndex)
+      console.log(this.currentClipIndex)
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new ClipPlayer()
 })
